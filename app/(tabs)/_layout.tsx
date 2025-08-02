2bbef5cb1d32a64b1794d5837a0316e69cbb4367
Júlio Cézar <jczrpereira77@gmail.com>
2025-08-02 17:41:15 -0300

    feat(tabs): add custom tab layout with navigation to Index and Profile screens

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,21 @@
+import { CustomTabs } from "@/components/CustomTabs";
+import { Tabs } from "expo-router";
+
+export default function AppLayout() {
+  return (
+    <Tabs tabBar={CustomTabs}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  );
+}

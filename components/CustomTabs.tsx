1cc68b039072c178b0d35d02f4a269de657bc5d1
Júlio Cézar <jczrpereira77@gmail.com>
2025-12-11 18:53:56 -0300

    refactor(CustomTabs): update icon sizes and colors to use theme object, remove unused label elements for cleaner UI

--- components/CustomTabs.tsx
+++ components/CustomTabs.tsx
@@ -1,45 +1,41 @@
-import { colors, font } from '@/constants/theme'
+import { theme } from '@/constants/theme'
 import type { BottomTabBarProps } from '@react-navigation/bottom-tabs'
 import * as Icons from 'phosphor-react-native'
 import { type ReactNode } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
 
 export function CustomTabs({ state, descriptors, navigation }: BottomTabBarProps) {
   const tabbarConfig: {
     [key: string]: {
       icon: (isFocused: boolean) => ReactNode
-      label: string
     }
   } = {
     index: {
       icon: (isFocused: boolean) => (
         <Icons.House
-          size={24}
-          color={isFocused ? colors.primary : '#AAAAAA'}
+          size={30}
+          color={isFocused ? theme.colors.primary : theme.colors.gray2}
           weight={isFocused ? 'fill' : 'regular'}
         />
       ),
-      label: 'Início',
     },
     explore: {
       icon: (isFocused: boolean) => (
         <Icons.MagnifyingGlass
-          size={24}
-          color={isFocused ? colors.primary : '#AAAAAA'}
+          size={30}
+          color={isFocused ? theme.colors.primary : theme.colors.gray2}
           weight={isFocused ? 'fill' : 'regular'}
         />
       ),
-      label: 'Explorar',
     },
     profile: {
       icon: (isFocused: boolean) => (
         <Icons.User
-          size={24}
-          color={isFocused ? colors.primary : '#AAAAAA'}
+          size={30}
+          color={isFocused ? theme.colors.primary : theme.colors.gray2}
           weight={isFocused ? 'fill' : 'regular'}
         />
       ),
-      label: 'Perfil',
     },
   }
 
@@ -81,9 +77,6 @@ export function CustomTabs({ state, descriptors, navigation }: BottomTabBarProps
             onLongPress={onLongPress}
             style={styles.tabbarItem}>
             {config.icon(isFocused)}
-            <Text style={styles.tabbarLabel}>
-              {config.label}
-            </Text>
           </TouchableOpacity>
         )
       })}
@@ -95,7 +88,7 @@ const styles = StyleSheet.create({
   tabbar: {
     flexDirection: 'row',
     width: '100%',
-    height: 70,
+    height: 50,
     justifyContent: 'space-around',
     alignItems: 'center',
     paddingBottom: 8,
@@ -106,11 +99,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     gap: 4,
-  },
-  tabbarLabel: {
-    fontSize: 12,
-    fontFamily: font.regular,
-    color: colors.gray2,
-    marginTop: 2,
   },
 })
